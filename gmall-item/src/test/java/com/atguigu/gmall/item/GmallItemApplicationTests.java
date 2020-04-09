63f0222da87e9a337b8befcc0f885b51b4414d3f
lifelinezhang <431370446@qq.com>
2020-04-09 23:08:43 +0800

    补充测试代码

---
+++ gmall-item/src/test/java/com/atguigu/gmall/item/GmallItemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallItemApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
